edf884e772c707c5ecfa1b3d2099df25035da593
deepfriedslipper <108072844+deepfriedslipper@users.noreply.github.com>
2025-04-10 22:23:23 +0100

    Update script.js

--- departments/language-and-culture/dictionary/script.js
+++ departments/language-and-culture/dictionary/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var row = tableBody.insertRow();
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
-        cell1.innerHTML = `<strong>${item.cheese}</strong> <span style="font-size: small;">${item.wordclass}</span><br>[${item.ipa}]`;
+        cell1.innerHTML = `<strong>${item.cheese}</strong> [${item.ipa}] <span style="font-size: small;">${item.wordclass}</span>`;
         cell2.textContent = item.english;
       });
     }
