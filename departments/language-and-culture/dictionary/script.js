d0ae3b1d10912d812d43b1385de218f3ac363481
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2025-04-11 18:35:48 +0100

    Update script.js

--- departments/language-and-culture/dictionary/script.js
+++ departments/language-and-culture/dictionary/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "words.json", true);
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function () 
     if (xhr.readyState == 4 && xhr.status == 200) {
       var data = JSON.parse(xhr.responseText);
       var tableBody = document.getElementById("data-table").getElementsByTagName("tbody")[0];
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var row = tableBody.insertRow();
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
-        cell1.innerHTML = `<strong class="cheesenese-writing", font-size=70>${item.cheese}</strong> [${item.ipa}] <span">${item.wordclass}</span>`;
+        cell1.innerHTML = `<strong class="cheesenese-writing", style="font-size:10px;" >${item.cheese}</strong> [${item.ipa}] <span style="font-size:small;">${item.wordclass}</span>`;
         cell2.textContent = item.english;
       });
     }
