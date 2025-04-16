d2df3830c6c89608c58a351c68205593fe3afddb
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2025-04-16 12:10:50 +0100

    Update script.js

--- departments/language-and-culture/dictionary/script.js
+++ departments/language-and-culture/dictionary/script.js
@@ -25,15 +25,16 @@ document.addEventListener("DOMContentLoaded", function () {
         if input[i] =="é"{
           input[i]=e;
         }
-        return input;
+        return input
       }
+    var noaccent=deaccent(filterValue);
     }
     rows.forEach(function (row) {
       var cells = row.getElementsByTagName("td");
       var found = false;
       for (var i = 0; i < cells.length; i++) {
         var cellText = cells[i].textContent.toLowerCase();
-        if (cellText.indexOf(filterValue) > -1) or (cellText.indexOf(deaccent(filterValue)) > -1) {
+        if (cellText.indexOf(filterValue) > -1) or (cellText.indexOf(noaccent) > -1) {
           found = true;
           break;
         }
