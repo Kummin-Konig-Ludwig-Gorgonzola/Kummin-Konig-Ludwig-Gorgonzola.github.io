1750a87628f6942bad843c6f0306b4315f3f4fa0
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2025-05-05 08:43:42 +0100

    Update rhymescript.js

--- departments/language-and-culture/dictionary/rhymescript.js
+++ departments/language-and-culture/dictionary/rhymescript.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
       data.forEach(function (item) {
         var row = tableBody.insertRow();
         var cell1 = row.insertCell(0);
+        console.log(item.cheese)
         cell1.innerHTML = `<strong class="cheesenese-writing">${item.cheese}</strong> • <span style="font-size: small">${item.cheese} • [${item.ipa}] • ${item.wordclass}</span>`;
         
       });
