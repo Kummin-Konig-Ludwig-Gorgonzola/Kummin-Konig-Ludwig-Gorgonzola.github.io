d070f0ee1e8f2225c9572a8198dc0f34cb01abee
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2025-05-06 11:52:31 +0100

    Update rhymescript.js

--- departments/language-and-culture/dictionary/rhymescript.js
+++ departments/language-and-culture/dictionary/rhymescript.js
@@ -61,8 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
            if (filterValue[filterValue.length-i-1]==cheese[cheese.length-i-1]){
            
             if (cheese[cheese.length-i-1]=="a"||cheese[cheese.length-i-1]=="e"||cheese[cheese.length-i-1]=="o"||cheese[cheese.length-i-1]=="i"||cheese[cheese.length-i-1]=="u"||cheese[cheese.length-i-1]=="ë"){
-              if (cheese[cheese.length-i-2]=="a"||cheese[cheese.length-i-2]=="e"||cheese[cheese.length-i-2]=="o"||cheese[cheese.length-i-2]=="i"||cheese[cheese.length-i-2]=="u"||cheese[cheese.length-i-2]=="ë"){
-                console.log(cheese[cheese.length-i-2],cheese[cheese.length-i-1])
+              if (filterValue[filterValue.length-i-2]=="a"||filterValue[filterValue.length-i-2]=="e"||filterValue[filterValue.length-i-2]=="o"||filterValue[filterValue.length-i-2]=="i"||filterValue[filterValue.length-i-2]=="u"||filterValue[filterValue.length-i-2]=="ë"){
               }
               else{
               found = true;
