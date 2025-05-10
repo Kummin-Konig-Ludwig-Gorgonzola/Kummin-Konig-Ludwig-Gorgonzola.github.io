356aa79fcf4ccba51a1bb09ba04a04fdf19d8c2d
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2025-05-10 16:24:56 +0100

    added zealification machine

---
+++ zealification-machine/script.js
@@ -0,0 +1,40 @@
+function activate(){
+    var ptexto = document.getElementById("ptext");
+    var plaintxt = ptexto.value;
+    var zealtxt = "";
+    
+    for (let i=0;i<plaintxt.length;i++){
+        if (plaintxt[i]=="i"){
+            zealtxt=zealtxt+"u";
+        }
+        else if (plaintxt[i]=="a"){
+            zealtxt=zealtxt+"e";
+        }
+        else if (plaintxt[i]=="e"){
+            zealtxt=zealtxt+"i";
+        }
+        else if (plaintxt[i]=="u"){
+            zealtxt=zealtxt+"a";
+        }
+        else if (plaintxt[i]=="I"){
+            zealtxt=zealtxt+"U";
+        }
+        else if (plaintxt[i]=="A"){
+            zealtxt=zealtxt+"E";
+        }
+        else if (plaintxt[i]=="E"){
+            zealtxt=zealtxt+"I";
+        }
+        else if (plaintxt[i]=="U"){
+            zealtxt=zealtxt+"A";
+        }
+        else{
+            zealtxt=zealtxt+plaintxt[i];
+        }
+    } 
+    ptexto=('<input type="text" class="mb-3 p-1 form-control w-auto" id="name" placeholder="'+zealtxt+'">');
+    
+    document.getElementById("ptext").value=zealtxt;
+    
+    return false
+}
